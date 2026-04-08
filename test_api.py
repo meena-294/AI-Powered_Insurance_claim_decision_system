546b8f2b888ed85c34eba236ae771b0ef7ad2c47
meena-294 <meenakshideiveegan294h@gmail.com>
2026-04-08 18:02:05 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,8 +1,12 @@
 import requests
 
-res = requests.post(
-    "http://127.0.0.1:7861/api/predict/state",
-    json={"data": []}
+# Step 1: Reset environment
+requests.post(
+    "http://127.0.0.1:8000/reset",
+    json={"task_level": "easy"}
 )
+
+# Step 2: Get state
+res = requests.get("http://127.0.0.1:8000/state")
 
 print(res.json())
